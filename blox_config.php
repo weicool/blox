398c27538c15a02e6b9170851fe5972ca027cc97
weicool <weicool@gmail.com>
2010-09-30 22:28:40 -0700

    Added blox_config.php. Death to security through obscurity.

---
+++ blox_config.php
@@ -0,0 +1,49 @@
+<?php
+
+$bloxConfig = array(
+    'db' => array(
+      'db' => 'd60650709',
+      'user' => 'u70718260',
+      'pass' => '2caa2f'
+    ),
+    'salt' => 'thisniSAcVERYlsalty94367'
+  );
+
+
+function validRecordRequest($score, $level, $cert, $ip, $salt, $db) {
+  if ($level == 0) {
+    $level = 1;
+  }
+  
+  /* Verify reasonable score. */
+  if ($score > ($level+1) * 250) return false;
+  
+  /* Minimum time needed should be at least 1 second/line. */
+  $minTimeNeeded = $level * 10;
+  
+  $sql = "DELETE FROM blox_certs
+          WHERE ip = '{$ip}' AND
+                MD5(CONCAT('{$salt}', `cert`)) = '{$cert}' AND
+                TIME_TO_SEC(TIMEDIFF(NOW(), `date`)) >= {$minTimeNeeded}";
+  mysql_query($sql, $db);
+  
+  return mysql_affected_rows($db) >= 1;
+}
+
+/** Make a certificate and record it for later. */
+function makeCert($db) {
+  $cert = strval(time());
+  $ip = mysql_real_escape_string(getIP(), $db);
+  
+  $sql = "INSERT INTO blox_certs (`cert`, `ip`, `date`)
+          VALUES ('{$cert}', '{$ip}', NOW())";
+  mysql_query($sql, $db);
+  
+  return $cert;
+}
+
+function encryptCert($cert, $salt) {
+  return md5($salt . $cert);
+}
+
+?>
